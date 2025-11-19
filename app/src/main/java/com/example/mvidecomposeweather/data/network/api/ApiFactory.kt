ad4197eb8e310d9045f0816e38894bc11eee4aa0
Tsyganov Viacheslav <vtsyganov@ujin.tech>
2025-11-19 20:17:28 +0500

    interceptor + apikey

--- app/src/main/java/com/example/mvidecomposeweather/data/network/api/ApiFactory.kt
+++ app/src/main/java/com/example/mvidecomposeweather/data/network/api/ApiFactory.kt
@@ -1,5 +1,7 @@
 package com.example.mvidecomposeweather.data.network.api
 
+import com.example.mvidecomposeweather.BuildConfig
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
@@ -8,9 +10,25 @@ object ApiFactory {
 
     private const val BASE_URL = "https://api.weatherapi.com/v1/"
 
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor { chain ->
+            val originalRequest = chain.request()
+            val newUrl = originalRequest
+                .url
+                .newBuilder()
+                .addQueryParameter("key", BuildConfig.WEATHER_API_KEY)
+                .build()
+            val newRequest = originalRequest
+                .newBuilder()
+                .url(newUrl)
+                .build()
+            chain.proceed(newRequest)
+        }.build()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttpClient)
         .build()
 
     val apiService: ApiService = retrofit.create()
--- app/src/main/java/com/example/mvidecomposeweather/data/network/api/ApiService.kt
+++ app/src/main/java/com/example/mvidecomposeweather/data/network/api/ApiService.kt
@@ -7,18 +7,18 @@ import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("current.json?key=")
+    @GET("current.json?")
     suspend fun loadCurrentWeather(
         @Query("q") query: String
     ): WeatherCurrentDto
 
-    @GET("forecast.json?key=")
+    @GET("forecast.json?")
     suspend fun loadForecast(
         @Query("q") query: String,
         @Query("days") daysCount: Int = 4
     ): WeatherCurrentDto
 
-    @GET("search.json?key=")
+    @GET("search.json?")
     suspend fun searchCity(
         @Query("q") query: String,
     ): List<CityDto>
