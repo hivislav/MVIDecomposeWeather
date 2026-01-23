aa58983d5f160124d31af3b28483d16837d8dc7b
Tsyganov Viacheslav <vtsyganov@ujin.tech>
2026-01-23 17:17:53 +0500

    Search screen

--- app/src/main/java/com/example/mvidecomposeweather/presentation/search/SearchContent.kt
+++ app/src/main/java/com/example/mvidecomposeweather/presentation/search/SearchContent.kt
@@ -1,11 +1,175 @@
 package com.example.mvidecomposeweather.presentation.search
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.example.mvidecomposeweather.presentation.details.DetailsComponent
-import com.example.mvidecomposeweather.presentation.favorite.FavoriteComponent
-import com.example.mvidecomposeweather.presentation.root.RootComponent
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.mvidecomposeweather.R
+import com.example.mvidecomposeweather.domain.entity.City
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchContent(component: SearchComponent) {
+    val state by component.state.collectAsState()
 
+    val focusRequester = remember {
+        FocusRequester()
+    }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+
+    SearchBar(
+        modifier = Modifier
+            .focusRequester(focusRequester = focusRequester),
+        query = state.searchQuery,
+        onQueryChange = {
+            component.changeSearchQuery(query = it)
+        },
+        onSearch = {
+            component.onClickSearch()
+        },
+        active = true,
+        onActiveChange = {},
+        leadingIcon = {
+            IconButton(
+                onClick = {
+                    component.onClickBack()
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = ""
+                )
+            }
+        },
+        trailingIcon = {
+            IconButton(
+                onClick = {
+                    component.onClickSearch()
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = ""
+                )
+            }
+        },
+        placeholder = {
+            Text(text = stringResource(R.string.search))
+        }
+    ) {
+        when (val searchState = state.searchState) {
+            is SearchStore.State.SearchState.EmptyResult -> {
+                Text(
+                    modifier = Modifier
+                        .padding(8.dp),
+                    text = stringResource(R.string.empty_result_text)
+                )
+            }
+
+            is SearchStore.State.SearchState.Error -> {
+                Text(
+                    modifier = Modifier
+                        .padding(8.dp),
+                    text = stringResource(R.string.error_result_text)
+                )
+            }
+
+            is SearchStore.State.SearchState.Initial -> {
+
+            }
+
+            is SearchStore.State.SearchState.Loading -> {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator(
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+            }
+
+            is SearchStore.State.SearchState.SuccessLoaded -> {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    contentPadding = PaddingValues(16.dp)
+                ) {
+                    items(
+                        items = searchState.cities,
+                        key = { it.id },
+                    ) { city ->
+                        CityCard(
+                            city = city,
+                            onCityClick = {
+                                component.onClickCity(city = city)
+                            }
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun CityCard(
+    city: City,
+    onCityClick: (City) -> Unit
+) {
+    Card(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { onCityClick.invoke(city) }
+                .padding(
+                    vertical = 8.dp,
+                    horizontal = 16.dp
+                )
+        ) {
+            Text(
+                text = city.name,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = city.country)
+        }
+    }
 }
